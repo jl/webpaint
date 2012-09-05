ffa1f1e74bb1384fc386c43bd3dd48756decce6e
Joey Liaw <joey@liaw.org>
2012-09-04 20:11:55 -0700

    save and restore images to app engine
    - add Layer model
    - add PUT handler for saving layers
    - insert saved layers directly into paint.html

--- main.py
+++ main.py
@@ -2,13 +2,22 @@
 
 # Standard library imports. These come with Python out of the box.
 import logging
+import random
 
 # App Engine-specific imports.
 import webapp2
+from google.appengine.ext import db
 from webapp2_extras import jinja2
 
 # Imports for our own code.
 import util
+
+
+class Layer(db.Model):
+    paint_id = db.StringProperty()
+    layer_id = db.StringProperty()
+    data_url = db.BlobProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -26,14 +35,34 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        paint_id = util.random_alphanum(5)
+        paint_id = util.random_alphanum(6)
         self.render_template('index.html', paint_id=paint_id)
 
 
 class PaintHandler(BaseHandler):
     def get(self, paint_id):
         logging.info('paint_id is ' + paint_id)
-        self.render_template('paint.html', paint_id=paint_id)
+        qry = Layer.all()
+        qry.filter('paint_id =', paint_id)
+        layers = []
+        for layer in qry:
+            layers.append(layer)
+        self.render_template('paint.html', paint_id=paint_id, layers=layers)
+
+
+class LayerHandler(BaseHandler):
+    def put(self, paint_id, layer_id):
+        logging.info('layer put: %s %s %s %d',
+            paint_id, layer_id,
+            self.request.headers['Content-Type'],
+            len(self.request.body))
+        data_url = self.request.body
+        if not data_url.startswith('data:image/png;base64,'):
+            self.error(400)
+            return
+        layer = Layer(paint_id=paint_id, layer_id=layer_id,
+                      data_url=self.request.body)
+        layer.put()
 
 
 # Handler is an array of tuples which maps URLs to request handler classes.
@@ -42,7 +71,8 @@ HANDLERS = [
     # The following regular expression only matches paint id's after /p/
     # composed entirely of letters, numbers, dash, or underscore, and with a
     # length between 3 and 100.
-    (r'/p/([A-Za-z0-9_-]{3,100})', PaintHandler),
+    (r'/p/(\w{,50})/', PaintHandler),
+    (r'/p/(\w{,50})/layer/([0-9.]{,20})', LayerHandler),
 ]
 
 
